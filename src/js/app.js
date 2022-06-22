4983ac8984c2d120767e989508075516fe92b44f
Анисимов Александр <ujc90@mail.ru>
2022-06-22 14:08:44 +0300

    edit hm

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,4 @@
 console.log('app worked');
+
+const game = new Game();
+game.start();
---
+++ src/js/game.js
@@ -0,0 +1,24 @@
+const domain = require('./domain');
+
+
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
+
+  module.exports={
+      Game,
+      GameSavingData,
+      readGameSaving,
+      writeGameSaving,
+    };
